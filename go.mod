5f6e4ab6512a797f954cd5b1f0377b2d7b88d362
amilaku <amila.kumaranayaka@gmail.com>
2019-11-15 23:14:48 +0100

    add fist code bits

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com:amila-ku/idampal
+
+go 1.12
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/valyala/fasttemplate v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20191112222119-e1110fd1c708 // indirect
+)
---
+++ main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+type (
+	property struct {
+		ID   int    `json:"id"`
+		Name string `json:"name"`
+		Seller string `json:"seller"`
+		Location string `json:"location"`
+		Url 	string `json: "url"`
+	}
+)
+
+var (
+	properties = map[int]*property{}
+	seq   = 1
+)
+
+//----------
+// Handlers
+//----------
+
+func createProperty(c echo.Context) error {
+	defer c.Request().Body.Close()
+
+	u := &property{
+		ID: seq,
+	}
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	properties[u.ID] = u
+	seq++
+	return c.JSON(http.StatusCreated, u)
+}
+
+func getProperty(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	return c.JSON(http.StatusOK, properties[id])
+}
+
+func getAllProperty(c echo.Context) error {
+	return c.JSON(http.StatusOK, properties)
+}
+
+func updateProperty(c echo.Context) error {
+	u := new(property)
+	if err := c.Bind(u); err != nil {
+		return err
+	}
+	id, _ := strconv.Atoi(c.Param("id"))
+	properties[id].Name = u.Name
+	return c.JSON(http.StatusOK, properties[id])
+}
+
+func deleteProperty(c echo.Context) error {
+	id, _ := strconv.Atoi(c.Param("id"))
+	delete(properties, id)
+	return c.NoContent(http.StatusNoContent)
+}
+
+func main() {
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.POST("/property", createProperty)
+	e.GET("/property", getAllProperty)
+	e.GET("/property/:id", getProperty)
+	e.PUT("/property/:id", updateProperty)
+	e.DELETE("/property/:id", deleteProperty)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
