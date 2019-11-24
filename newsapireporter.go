22288c45363e866b30825bbf0f2bf16a641eb0f8
amilaku <amila.kumaranayaka@gmail.com>
2019-11-24 22:10:11 +0100

    add reporter

--- newsapireporter.go
+++ newsapireporter.go
@@ -13,5 +13,22 @@ type NewsAPIResponce struct {
 // GetNewsArticles gets news
 func (a NewsAPIResponce) GetNewsArticles () []Article {
 	art := []Article{}
+
+	search.NextPage = next
+	pageSize := 20
+
+	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%d&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(search.SearchKey), pageSize, search.NextPage, *apiKey)
+	resp, err := http.Get(endpoint)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
 	return art
 }
