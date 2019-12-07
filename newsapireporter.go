d267033d6fd65e86b11001daabddde234cc7c326
amilaku <amila.kumaranayaka@gmail.com>
2019-12-08 00:30:09 +0100

    news reporter

--- newsapireporter.go
+++ newsapireporter.go
@@ -1,7 +1,26 @@
 package main
 
+import (
+	"io/ioutil"
+	"log"
+	"fmt"
+	"net/url"
+	"net/http"
+	"encoding/json"
+	"math"
+	//"github.com/labstack/gommon/log"
 
-//type articles	[]Article
+)
+
+// Search defines user search 
+type Search struct {
+	SearchKey  string
+	ApiKey 	   string
+	PageSize   int
+	NextPage   int
+	TotalPages int
+	Results    NewsAPIResponce
+}
 
 // NewsAPIResponce defines responce structure from news api 
 type NewsAPIResponce struct {
@@ -10,25 +29,49 @@ type NewsAPIResponce struct {
 	NewsArticles []Article  `json: "articles"`	
 }
 
-// GetNewsArticles gets news
-func (a NewsAPIResponce) GetNewsArticles () []Article {
-	art := []Article{}
+// NewSearch intiates a search
+func NewSearch(apiKey string, sKey string) Search {
+	search := Search{}
 
-	search.NextPage = next
-	pageSize := 20
+	search.SearchKey = sKey
+	search.NextPage = 1
+	search.PageSize = 2
+	search.ApiKey = apiKey
 
-	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%d&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(search.SearchKey), pageSize, search.NextPage, *apiKey)
+	return search
+}
+
+// GetNewsArticles gets news from news api
+func (s Search) GetNewsArticles () (Search, error) {
+
+	endpoint := fmt.Sprintf("https://newsapi.org/v2/everything?q=%s&pageSize=%d&page=%d&apiKey=%s&sortBy=publishedAt&language=en", url.QueryEscape(s.SearchKey), s.PageSize, s.NextPage, s.ApiKey)
+	//fmt.Println(endpoint)
 	resp, err := http.Get(endpoint)
+
+	//fmt.Println(resp)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		log.Fatalln("unable to reach url")
 	}
 
 	defer resp.Body.Close()
 
 	if resp.StatusCode != 200 {
-		w.WriteHeader(http.StatusInternalServerError)
-		return
+		log.Fatalln("not succesfull")
 	}
-	return art
+
+	body, err := ioutil.ReadAll(resp.Body)
+	
+	//fmt.Println(body)
+	
+	err = json.Unmarshal(body,&s.Results)
+
+	fmt.Println(s.Results)
+
+	if err != nil {
+		log.Fatalln("parsing failed")
+	}
+
+	s.TotalPages = int(math.Ceil(float64(s.Results.TotalResults / s.PageSize)))
+	
+	return s, err
 }
